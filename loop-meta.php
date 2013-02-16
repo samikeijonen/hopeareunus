aafe6059e0ddfe13ffa500932ba1fd3c57786b97
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-02-16 10:21:03 +0200

    Add html markup directly in loop meta.

--- loop-meta.php
+++ loop-meta.php
@@ -13,126 +13,180 @@
 ?>
 
 	<?php if ( is_home() && !is_front_page() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap"> 
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php echo get_post_field( 'post_title', get_queried_object_id() ); ?></h1>
+					<h1 class="loop-title"><?php echo get_post_field( 'post_title', get_queried_object_id() ); ?></h1>
 			
-			<?php if( has_excerpt( get_queried_object_id() ) ) { ?>
-				<div class="loop-description">
-					<?php echo apply_filters( 'the_excerpt', get_post_field( 'post_excerpt', get_queried_object_id() ) ); ?>
-				</div><!-- .loop-description -->
-			<?php } // End if ?>
+					<?php if( has_excerpt( get_queried_object_id() ) ) { ?>
+						<div class="loop-description">
+							<?php echo apply_filters( 'the_excerpt', get_post_field( 'post_excerpt', get_queried_object_id() ) ); ?>
+						</div><!-- .loop-description -->
+					<?php } // End if ?>
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_category() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap"> 
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php single_cat_title(); ?></h1>
+					<h1 class="loop-title"><?php single_cat_title(); ?></h1>
 
-			<div class="loop-description">
-				<?php echo category_description(); ?>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<?php echo category_description(); ?>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_tag() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap"> 
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php single_tag_title(); ?></h1>
+					<h1 class="loop-title"><?php single_tag_title(); ?></h1>
 
-			<div class="loop-description">
-				<?php echo tag_description(); ?>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<?php echo tag_description(); ?>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_tax() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap"> 
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php single_term_title(); ?></h1>
+					<h1 class="loop-title"><?php single_term_title(); ?></h1>
 
-			<div class="loop-description">
-				<?php echo term_description( '', get_query_var( 'taxonomy' ) ); ?>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<?php echo term_description( '', get_query_var( 'taxonomy' ) ); ?>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_author() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap"> 
 
-		<?php $id = get_query_var( 'author' ); ?>
+				<?php $id = get_query_var( 'author' ); ?>
 
-		<div id="hcard-<?php echo esc_attr( get_the_author_meta( 'user_nicename', $id ) ); ?>" class="loop-meta author-profile vcard">
+				<div id="hcard-<?php echo esc_attr( get_the_author_meta( 'user_nicename', $id ) ); ?>" class="loop-meta author-profile vcard">
 
-			<div class="loop-description">
+					<div class="loop-description">
 				
-				<?php echo get_avatar( get_the_author_meta( 'user_email', $id ), '66', '', get_the_author_meta( 'display_name', $id ) ); ?>
+						<?php echo get_avatar( get_the_author_meta( 'user_email', $id ), '66', '', get_the_author_meta( 'display_name', $id ) ); ?>
 				
-				<h1 class="loop-title fn n"><?php the_author_meta( 'display_name', $id ); ?></h1>
+						<h1 class="loop-title fn n"><?php the_author_meta( 'display_name', $id ); ?></h1>
 
-				<?php echo wpautop( get_the_author_meta( 'description', $id ) ); ?>
+						<?php echo wpautop( get_the_author_meta( 'description', $id ) ); ?>
 
-			</div><!-- .loop-description -->
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_search() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap"> 
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php echo esc_attr( get_search_query() ); ?></h1>
+					<h1 class="loop-title"><?php echo esc_attr( get_search_query() ); ?></h1>
 
-			<div class="loop-description">
-				<p>
-				<?php printf( __( 'You are browsing the search results for &quot;%1$s&quot;', 'hopeareunus' ), esc_attr( get_search_query() ) ); ?>
-				</p>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<p>
+						<?php printf( __( 'You are browsing the search results for &quot;%1$s&quot;', 'hopeareunus' ), esc_attr( get_search_query() ) ); ?>
+						</p>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_date() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap">
 
-		<div class="loop-meta">
-			<h1 class="loop-title"><?php _e( 'Archives by date', 'hopeareunus' ); ?></h1>
+				<div class="loop-meta">
+					<h1 class="loop-title"><?php _e( 'Archives by date', 'hopeareunus' ); ?></h1>
 
-			<div class="loop-description">
-				<p>
-				<?php _e( 'You are browsing the site archives by date.', 'hopeareunus' ); ?>
-				</p>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<p>
+						<?php _e( 'You are browsing the site archives by date.', 'hopeareunus' ); ?>
+						</p>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_post_type_archive() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap">
 
-		<?php $post_type = get_post_type_object( get_query_var( 'post_type' ) ); ?>
+				<?php $post_type = get_post_type_object( get_query_var( 'post_type' ) ); ?>
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php post_type_archive_title(); ?></h1>
+					<h1 class="loop-title"><?php post_type_archive_title(); ?></h1>
 
-			<div class="loop-description">
-				<?php if ( !empty( $post_type->description ) ) echo "<p>{$post_type->description}</p>"; ?>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<?php if ( !empty( $post_type->description ) ) echo "<p>{$post_type->description}</p>"; ?>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php elseif ( is_archive() ) : ?>
+	
+		<div id="hopeareunus-header-title">
+			<div class="wrap">
 
-		<div class="loop-meta">
+				<div class="loop-meta">
 
-			<h1 class="loop-title"><?php _e( 'Archives', 'hopeareunus' ); ?></h1>
+					<h1 class="loop-title"><?php _e( 'Archives', 'hopeareunus' ); ?></h1>
 
-			<div class="loop-description">
-				<p>
-				<?php _e( 'You are browsing the site archives.', 'hopeareunus' ); ?>
-				</p>
-			</div><!-- .loop-description -->
+					<div class="loop-description">
+						<p>
+						<?php _e( 'You are browsing the site archives.', 'hopeareunus' ); ?>
+						</p>
+					</div><!-- .loop-description -->
 
-		</div><!-- .loop-meta -->
+				</div><!-- .loop-meta -->
+				
+			</div><!-- .wrap -->
+		</div><!-- #hopeareunus-header-title -->
 
 	<?php endif; ?>
