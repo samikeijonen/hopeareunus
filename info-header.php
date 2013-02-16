37b685b30a54276a974559c35babaca140fa4003
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-02-16 10:22:32 +0200

    Put html markup in loop meta file.

--- info-header.php
+++ info-header.php
@@ -11,20 +11,16 @@
  */
 ?>
 				
-	<?php if ( !is_home() ) {
+	<?php
 				
-			if( is_singular( apply_filters( 'hopeareunus_show_excerpt_header_1' , array( 'page' ) ) ) && is_main_query() && has_excerpt( get_queried_object_id() ) ) { ?>
-				<div id="hopeareunus-header-title">
-					<div class="wrap"> 
-						<?php the_excerpt(); ?>
-					</div><!-- .wrap -->
-				</div><!-- #hopeareunus-header-title -->
-			<?php } elseif ( !is_singular( apply_filters( 'hopeareunus_show_excerpt_header_2' , array( 'page', 'post' ) ) ) ) { ?>
-				<div id="hopeareunus-header-title">
-					<div class="wrap"> 
-						<?php get_template_part( 'loop-meta' ); // Loads the loop-meta.php template. ?>	
-					</div><!-- .wrap -->
-				</div><!-- #hopeareunus-header-title -->
-			<?php } // End else
-	} // End if
+		if( is_singular( apply_filters( 'hopeareunus_show_excerpt_header_1' , array( 'page', 'portfolio_item' ) ) ) && is_main_query() && has_excerpt( get_queried_object_id() ) ) { ?>
+			<div id="hopeareunus-header-title">
+				<div class="wrap"> 
+					<?php the_excerpt(); ?>
+				</div><!-- .wrap -->
+			</div><!-- #hopeareunus-header-title -->
+		<?php } else {
+					get_template_part( 'loop-meta' ); // Loads the loop-meta.php template.
+				}
+				
 	?>
