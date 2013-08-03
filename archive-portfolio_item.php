3da3cd65982c726ebf4fffdfc7957f977c3c3eb6
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-08-03 23:53:09 +0300

    Header title changed.

--- archive-portfolio_item.php
+++ archive-portfolio_item.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Index Template
+ * Archive Portfolio Template
  *
- * This is the default template. It is used when a more specific template can't be found to display
- * posts. It is unlikely that this template will ever be used, but there may be rare cases.
+ * This is the portfolio archive template and it is used to show off your portfolio.
  *
  * @package    Hopeareunus
  * @subpackage Template
